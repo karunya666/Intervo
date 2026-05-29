df0095eac7b6f4baff7a9ae3ff426f5c7132e761
karunya666 <karunyaribu@gmail.com>
2026-05-29 20:03:43 +0530

    session route & controllers added

--- backend/src/server.js
+++ backend/src/server.js
@@ -8,6 +8,7 @@ import { serve } from 'inngest/express';
 import { inngest, functions } from '../lib/inngest.js';
 import { clerkMiddleware } from '@clerk/express';
 import chatRoutes from '../routes/chatRoutes.js';
+import sessionRoutes from '../routes/sessionRoute.js';
 
 dotenv.config();
 
@@ -23,6 +24,7 @@ app.use(clerkMiddleware()); // this adds auth field to request object: req.auth(
 
 app.use('/api/inngest', serve({ client: inngest, functions }));
 app.use('/api/chat', chatRoutes);
+app.use('/api/sessions', sessionRoutes);
 
 app.get('/health', (req, res) => {
   res.status(200).json({ msg: 'API is up and running' });
